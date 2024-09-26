1078b26b17e72aec03652e38ed04db150ea6cfdb
liliyom <liliyoumkarim@gmail.com>
2024-09-26 15:04:13 +0500

    heaer and home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,23 @@ const Home = () => {
       <div className="font-bold p-4 ">
         <h2>Welcome To The Health Care</h2>
       </div>
+      <div>
+        <h1 className="font-bold p-4 text-color-white text-5xl">
+          Navigating The Department Of The Health Care
+        </h1>
+      </div>
+      <div className="font-bold p-4 text-color-white text-base">
+        <p>
+          Health care refers to the maintenance and improvement of physical and
+          mental health through the prevention, diagnosis, treatment, and
+          recovery from illness or injury.
+        </p>
+      </div>
+      <div>
+        <button className="bg-white text-red-900 font-bold py-2 px-4 rounded hover:bg-red-900 hover:text-white border border-white">
+          CHAT-BOT
+        </button>
+      </div>
     </div>
   );
 };
