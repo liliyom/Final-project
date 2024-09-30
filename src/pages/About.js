d62ba44e90f3516d44b2e766c40ff247d3154c9e
liliyom <liliyoumkarim@gmail.com>
2024-09-30 15:19:51 +0500

    about done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../layout/header";
+import Footer from "../layout/Footer";
+
 
 const About = () => {
   return (
     <div>
-      about page
+      <Header></Header>
+      <div className="  h-screen bg-cover bg-center  bg-[url('/public/image/img2.jpg')] bg-transparent h-60">
+        <h1 className="p-4 text-7xl	 font-bold tracking-wide">About Us</h1>
+      </div>
+      <div>
+        <h2 className="font-bold text-3xl justify">Health Care</h2>
+        <h1>Compassionate Health Care for All</h1>
+        <p>
+          At [Your Organization Name], we are dedicated to providing
+          compassionate, patient-centered health care for individuals of all
+          ages and backgrounds. Our team of experienced professionals is
+          committed to delivering high-quality medical services with empathy,
+          respect, and a focus on holistic well-being. Whether you need routine
+          check-ups, specialized treatments, or ongoing support, we are here to
+          ensure your health and comfort are our top priorities. We believe that
+          every person deserves access to excellent care, and we strive to
+          create a welcoming environment where your health is in trusted hands.
+        </p>
+      </div>
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,18 @@
 import React from "react";
+import Footer from "../layout/Footer";
 
 const Home = () => {
   return (
-    <div className="h-screen bg-cover bg-center bg-[url('/public/image/img1.jfif')]">
-      <div className="font-bold p-4 ">
+    <div className="h-screen bg-cover bg-center  bg-[url('/public/image/img2.jpg')] bg-transparent h-96">
+      <div className="font-bold p-4 text-red-900 font-sans text-lg tracking-wide">
         <h2>Welcome To The Health Care</h2>
       </div>
       <div>
-        <h1 className="font-bold p-4 text-color-white text-5xl">
+        <h1 className="font-bold p-4 text-color-white text-5xl text-red-900 font-sans tracking-wide ">
           Navigating The Department Of The Health Care
         </h1>
       </div>
-      <div className="font-bold p-4 text-color-white text-base">
+      <div className="font-bold p-4 text-color-white text-base text-red-900 font-sans text-lg tracking-wide">
         <p>
           Health care refers to the maintenance and improvement of physical and
           mental health through the prevention, diagnosis, treatment, and
@@ -19,9 +20,15 @@ const Home = () => {
         </p>
       </div>
       <div>
-        <button className="bg-white text-red-900 font-bold py-2 px-4 rounded hover:bg-red-900 hover:text-white border border-white">
-          CHAT-BOT
-        </button>
+        <div className="flex justify-center items-start  ">
+          <button className="bg-white text-red-900 font-bold  px-4 py-2 mr-8 rounded hover:bg-red-900 hover:text-white border border-white ">
+            CHAT-BOT
+          </button>
+          <button className="bg-white text-red-900 font-bold px-4 py-2 rounded hover:bg-red-900 hover:text-white border border-white">
+            MAKE APPOINTMENT
+          </button>
+          <Footer></Footer>
+        </div>
       </div>
     </div>
   );
